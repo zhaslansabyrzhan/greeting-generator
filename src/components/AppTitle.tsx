8dc009a8cbe6723c68481eb130f31e661407907c
Sabyrzhan Zhaslanuly <sabyrzhanzhaslan@outlook.com>
2026-07-11 21:06:07 +0500

    feat: add app title component

---
+++ src/components/AppTitle.tsx
@@ -0,0 +1,13 @@
+export const AppTitle = () => {
+  return (
+    <div className="mb-10 sm:mb-16">
+      <h2 className="text-4xl sm:text-5xl font-extrabold text-gray-900 leading-tight mb-4">
+        Создайте <span className="text-purple-600">уникальное</span>
+        <br className="hidden sm:block" /> поздравление
+      </h2>
+      <p className="text-gray-500 text-lg max-w-2xl">
+        Выберите праздник, введите имя и магия начнется! Наш ИИ создаст персональное пожелание за секунды.
+      </p>
+    </div>
+  )
+}
